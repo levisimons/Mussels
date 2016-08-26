b6af8baf1a6a36fb8ee7a6ad0c97158db1659328
Ariel Levi Simons <levisimons@gmail.com>
2016-08-26 16:05:56 -0700

    Update MutationCounter.py

--- MutationCounter.py
+++ MutationCounter.py
@@ -1,25 +1,30 @@
 import os,sys,re
 import csv
 
-#Open a file containing the allele frequencies at various SNP sites.
-fileInput = "Juveniles.frq"
-genomeNum = 60
-fileOutput = ("JuvenileOutliers.tsv")
-header = ["CHROM","POS","N_ALLELES","N_CHR","ALLELE_FREQ1","ALLELE_FREQ2","ALLELE_FREQ3","ALLELE_FREQ4"]
-output = csv.writer(open(fileOutput, "w"),delimiter='\t')
-output.writerow(header)
+fileInput = "SOL_MYTILUS.VF.SNP.vcf"
+IndividualNum = 30
 
-# Look for SNPs where at least half of the individuals are present, scan the relative frequency of the
-# alleles, then select the SNPs where one of the alleles is only present at a frequency at or below a
-# certain threshold.
-with open(fileInput, "rU") as f:
-    f = csv.reader(f, delimiter="\t")
-    next(f, None)
-    for line in f:
-        if [] == line: break
-        if float(line[3]) >= float(genomeNum/2):
-            alleleNum = int(line[2])
-            threshold = 1/float(line[3])
-            for i in range (0, alleleNum):
-                if float(line[4+i].split(":")[1]) <= threshold and float(line[4+i].split(":")[1]) > 0:
-                    output.writerow(line)
+#Open the vcf file containing all the loci where potential SNPs are located.
+#Filter out the headers.
+#Filter out the adults genome columns as well as the loci metadata columns.
+#Filter out loci with less than half of the individuals counted.
+#Check for individuals with a unique genotype at that loci.
+#Count up each instance for each individual.
+f = csv.reader(open(fileInput,"rU"), delimiter='\t')
+genotype = []
+mutationCount = [0]*IndividualNum
+for line in f:
+    if line[0].startswith('##') == False:
+        for i in range(0, len(line)):
+            if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
+                if line[i].split(':')[0] != "./.":
+                    genotype.append(line[i].split(':')[0])
+        genotypeFreq = dict((j, genotype.count(j)) for j in genotype)
+        if len(genotype) >= IndividualNum/2:
+            rareGenotype = [x for x in genotype if genotype.count(x) == 1]
+            if len(rareGenotype) > 0:
+                for i in range(0,len(rareGenotype)):
+                    mutationIndex = genotype.index(rareGenotype[i])
+                    mutationCount[mutationIndex] = mutationCount[mutationIndex]+1
+        genotype = []
+print mutationCount
