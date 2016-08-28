a707947c1bcf13ac64bd011738264a3820bf73d1
Ariel Levi Simons <levisimons@gmail.com>
2016-08-28 15:07:10 -0700

    Update MutationCounter.py

--- MutationCounter.py
+++ MutationCounter.py
@@ -2,29 +2,34 @@ import os,sys,re
 import csv
 
 fileInput = "SOL_MYTILUS.VF.SNP.vcf"
-IndividualNum = 30
+GenotypeNum = 30
 
 #Open the vcf file containing all the loci where potential SNPs are located.
 #Filter out the headers.
 #Filter out the adults genome columns as well as the loci metadata columns.
 #Filter out loci with less than half of the individuals counted.
-#Check for individuals with a unique genotype at that loci.
+#Check for individuals with a unique haplotype at that loci.
 #Count up each instance for each individual.
 f = csv.reader(open(fileInput,"rU"), delimiter='\t')
 genotype = []
-mutationCount = [0]*IndividualNum
+haplotype = []
+mutationCount = [0]*GenotypeNum
 for line in f:
-    if line[0].startswith('##') == False:
+    if line[0].startswith('#') == False:
         for i in range(0, len(line)):
             if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
                 if line[i].split(':')[0] != "./.":
                     genotype.append(line[i].split(':')[0])
-        genotypeFreq = dict((j, genotype.count(j)) for j in genotype)
-        if len(genotype) >= IndividualNum/2:
-            rareGenotype = [x for x in genotype if genotype.count(x) == 1]
-            if len(rareGenotype) > 0:
-                for i in range(0,len(rareGenotype)):
-                    mutationIndex = genotype.index(rareGenotype[i])
-                    mutationCount[mutationIndex] = mutationCount[mutationIndex]+1
+                    haplotype.append(line[i].split(':')[0].split('/')[0])
+                    haplotype.append(line[i].split(':')[0].split('/')[1])
+        if len(genotype) >= GenotypeNum/2:
+            rareHaplotype = [x for x in haplotype if haplotype.count(x) == 1]
+            if len(rareHaplotype) > 0:
+                for i in range(0,len(rareHaplotype)):
+                    Index = int(haplotype.index(rareHaplotype[i])/2)
+                    mutationCount[Index] = mutationCount[Index]+1
+                print rareHaplotype,Index,haplotype
+                print mutationCount
+        haplotype = []
         genotype = []
 print mutationCount
