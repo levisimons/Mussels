accd00ba32085c512d352962d7cb7e1552ef2bfd
Ariel Levi Simons <levisimons@gmail.com>
2016-08-30 13:22:28 -0700

    Update MutationCounter.py

--- MutationCounter.py
+++ MutationCounter.py
@@ -14,22 +14,37 @@ f = csv.reader(open(fileInput,"rU"), delimiter='\t')
 genotype = []
 haplotype = []
 mutationCount = [0]*GenotypeNum
+
+#output = csv.writer(open("JuvenileMutations.vcf","w"), delimiter='\t')
+
 for line in f:
+#    if line[0].startswith('#') == True and line[0].startswith('##') == False:
+#        output.writerow(line)
     if line[0].startswith('#') == False:
-        for i in range(0, len(line)):
+        j=0
+	for i in range(0, len(line)):
             if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
                 if line[i].split(':')[0] != "./.":
-                    genotype.append(line[i].split(':')[0])
-                    haplotype.append(line[i].split(':')[0].split('/')[0])
-                    haplotype.append(line[i].split(':')[0].split('/')[1])
-        if len(genotype) >= GenotypeNum/2:
-            rareHaplotype = [x for x in haplotype if haplotype.count(x) == 1]
-            if len(rareHaplotype) > 0:
-                for i in range(0,len(rareHaplotype)):
-                    Index = int(haplotype.index(rareHaplotype[i])/2)
+                    j=j+1
+                genotype.append(line[i].split(':')[0])
+                haplotype.append(line[i].split(':')[0].split('/')[0])
+                haplotype.append(line[i].split(':')[0].split('/')[1])
+        rareGenotype = [x for x in genotype if genotype.count(x) == 1]
+        rareHaplotype = [x for x in haplotype if haplotype.count(x) == 1]
+        if len(rareGenotype) > 0 and len(rareHaplotype) == 0:
+            for i in range(0,len(rareGenotype)):
+                if rareGenotype != ['./.']: Index = int(genotype.index(rareGenotype[i]))
+                if j >= 15 and rareGenotype != ['./.']:
                     mutationCount[Index] = mutationCount[Index]+1
-                print rareHaplotype,Index,haplotype
-                print mutationCount
-        haplotype = []
-        genotype = []
+                    print 'Single genotype',j,len(genotype),rareGenotype,Index,genotype
+                    print mutationCount
+        if len(rareHaplotype) > 0 and len(rareGenotype) == 0:
+            for i in range(0,len(rareHaplotype)):
+                if rareHaplotype != ['./.']: Index = int(haplotype.index(rareHaplotype[i])/2)
+                if j >= 15 and rareHaplotype != ['.']:
+                    mutationCount[Index] = mutationCount[Index]+1
+                    print 'Single haplotype',j,len(haplotype),rareHaplotype,Index,haplotype
+                    print mutationCount
+        genotype=[]
+        haplotype=[]
 print mutationCount
