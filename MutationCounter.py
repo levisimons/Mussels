dfb1dba68ac0c9f82fe8d7c2cc70050692d3a737
Ariel Levi Simons <levisimons@gmail.com>
2016-09-03 07:04:52 -0700

    Update MutationCounter.py

--- MutationCounter.py
+++ MutationCounter.py
@@ -9,15 +9,13 @@ GenotypeNum = 30
 #Filter out the adults genome columns as well as the loci metadata columns.
 #Filter out loci with less than half of the individuals counted.
 #Check for individuals with a unique haplotype at that loci.
+#Check for individuals with a unique homozygotic genotype at that loci.
 #Count up each instance for each individual.
 f = csv.reader(open(fileInput,"rU"), delimiter='\t')
 genotype = []
 haplotype = []
 mutationCount = [0]*GenotypeNum
 
-#output = csv.writer(open("JuvenileMutations.vcf","w"), delimiter='\t')
-
-lc = 0
 for line in f:
     if line[0].startswith('#') == False:
         j=0
