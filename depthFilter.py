41ed1dfb954db39c6140d14b9208a9c8e5f2e9c9
Ariel Levi Simons <levisimons@gmail.com>
2016-09-30 15:12:02 -0700

    Update depthFilter.py

--- depthFilter.py
+++ depthFilter.py
@@ -2,7 +2,7 @@ import os,sys,re
 import csv
 
 fileInput = "SOL_MYTILUS.VF.SNP.vcf"
-GenotypeNum = 30
+fileOutput = "SOL_MYTILUS.VF.SNP.Filtergt4.vcf"
 
 #Open the vcf file containing all the loci where potential SNPs are located.
 #Filter out the headers.
@@ -12,15 +12,12 @@ GenotypeNum = 30
 #Check for individuals with a unique homozygotic genotype at that loci.
 #Count up each instance for each individual.
 f = csv.reader(open(fileInput,"rU"), delimiter='\t')
-genotype = []
-haplotype = []
-mutationCount = [0]*GenotypeNum
 
 for line in f:
     if line[0].startswith('#') == False:
+	outline = []
 	for i in range(0, len(line)):
-		outline = []
-		outline[i].append(line[i])
+		outline.append(line[i])
 	for i in range(0,len(outline)):
 		if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
 			DP = line[i].split(':')[2]
