e2acd26be72ff6e60a0a68d39a96b9cc90490d07
Ariel Levi Simons <levisimons@gmail.com>
2016-09-30 14:32:00 -0700

    Update depthFilter.py

--- depthFilter.py
+++ depthFilter.py
@@ -20,5 +20,5 @@ for line in f:
     if line[0].startswith('#') == False:
 	for i in range(0, len(line)):
             if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
-		print line[i]
+		print line[i].split(':')[2]
                 
