4b6e3309c159d65ad0bb8ff0875ed7d89ae29e2d
Ariel Levi Simons <levisimons@gmail.com>
2016-09-30 12:20:17 -0700

    Update ReadDepth.py

--- ReadDepth.py
+++ ReadDepth.py
@@ -17,19 +17,16 @@ def depthGraph(index):
     ax.set_ylabel('Relative abundance')
     ax.set_xlim(0, 20)
     plt.show()
-    filename = header[index],pdf
+    filename = header[index],'pdf'
     filename = '.'.join(filename)
     pp = PdfPages(filename)
     pp.savefig(fig)
     pp.close()
     f.close()
 
-header = ['CHROM','POS','12x10_FP','12x10_J01','12x10_J02','12x10_J03','12x10_J04','12x10_J05','12x10_J06','12x10_J07','12x10_J08','12x10_J09','12x10_J10','12x10_MP','14x12_FP','14x12_J01','14x12_J02','14x12_J03','14x12_J04','14x12_J05','14x12_J06','14x12_J07','14x12_J08','14x12_J09','14x12_J10','14x12_MP','8x6_FP','8x6_J01','8x6_J02','8x6_J03','8x6_J04','8x6_J05','8x6_J06','8x6_J07','8x6_J08','8x6_J09','8x6_J10','8x6_MP']
+header = ['CHROM','POS','12x10_FP','12x10_J01','12x10_J02','12x10_J03','12x10_J04','12x10_J05','12x10_J06','12x10_J07','12x10_J08','12x10_J09','12x10_J10','12x10_MP','14x12_FP','14x12_J01','14x12_J02','14x12_J03','14x12_J04','14x12_J05'$
 
 for i in range(2,len(header)):
     print i,header[i]
     index = i
-    if index == 2:
-        depthGraph(index)
-
-
+    depthGraph(index)
