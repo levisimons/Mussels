7a8e58e8635b1ee85e92e5173fe0be9881a77904
Ariel Levi Simons <levisimons@gmail.com>
2016-09-30 14:51:32 -0700

    Update depthFilter.py

--- depthFilter.py
+++ depthFilter.py
@@ -19,9 +19,12 @@ mutationCount = [0]*GenotypeNum
 for line in f:
     if line[0].startswith('#') == False:
 	for i in range(0, len(line)):
-            if i not in (0,1,2,3,4,5,6,7,8,9,20,21,32,33,44):
+		outline = []*len(line)
+		outline[i] = line[i]
 		DP = line[i].split(':')[2]
 		if DP != '.':
-			if DP < 4:
-				print line
+			if int(DP) < 4:
+				outline[i] = './.:.:.:.:.'
+	print line
+	print outline
                 
